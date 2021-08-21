e92343bc260887cbb2584f54575fe6d6d435544d
lhake17 <laurin-hake97@outlook.com>
2021-08-21 22:57:47 +0200

    MainController created
    TabBarController setup

---
+++ RemBDayApp/RemBDayApp/MainTabBarController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class MainTabBarController: UITabBarController{
+
+    @IBOutlet weak var tabbar: UITabBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
---
+++ RemBDayApp/RemBDayApp/WeekPageController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class WeekPageController: UIViewController {
+
+    @IBOutlet weak var weektab: UITabBarItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+
+
+}
+
